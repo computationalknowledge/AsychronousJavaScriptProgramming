b8ee79f85e6c26559566c7f17212201193bdf008
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-06-16 08:46:59 -0400

    1
    1

--- setTimeOut.js
+++ setTimeOut.js
@@ -14,4 +14,16 @@ setTimeout(function() {
    console.log("After timeout: " + new Date());
 }, 60*1000);
 
-Brown, Ethan. Learning JavaScript . O'Reilly Media. Kindle Edition. 
+
+// another example:
+
+const start = new Date();
+let i=0;
+const intervalId = setInterval(function() {
+   let now = new Date();
+   if(now.getMinutes() !== start.getMinutes() || ++i>10)
+      return clearInterval(intervalId);
+   console.log(`${i}: ${now}`);
+}, 5*1000);
+
+

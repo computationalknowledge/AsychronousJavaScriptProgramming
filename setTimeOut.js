46f6481d7f072132fd55dd0cc7cbb737db176649
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-06-16 08:36:30 -0400

    2
    2

--- setTimeOut.js
+++ setTimeOut.js
@@ -6,3 +6,12 @@ setTimeout(f, 60*1000); // one minute
 console.log("I happen after setTimeout!");
 console.log("Me too!");
 
+
+
+// another example
+
+setTimeout(function() {
+   console.log("After timeout: " + new Date());
+}, 60*1000);
+
+Brown, Ethan. Learning JavaScript . O'Reilly Media. Kindle Edition. 

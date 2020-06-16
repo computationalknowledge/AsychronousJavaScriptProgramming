520bbe6a9b885b82a0e638cd63b813c9e999af10
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-06-16 09:04:13 -0400

    1
    1

--- setTimeOut.js
+++ setTimeOut.js
@@ -26,4 +26,18 @@ const intervalId = setInterval(function() {
    console.log(`${i}: ${now}`);
 }, 5*1000);
 
+//another example:
+
+var myVar = setInterval(myTimer, 1000);
+
+function myTimer() {
+  var d = new Date();
+  var t = d.toLocaleTimeString();
+  document.getElementById("demo").innerHTML = t;
+}
+
+function myStopFunction() {
+  clearInterval(myVar);
+}
+
 
